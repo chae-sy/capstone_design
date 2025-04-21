c3b1e562ab4093691f06283fc810f803c9da9dac
milkaroo <thlrla03@naver.com>
2025-04-21 18:17:42 +0900

    Update controller.v

--- Verilog/controller.v
+++ Verilog/controller.v
@@ -1,1 +1,25 @@
 //controller
+module controller (
+
+)
+
+memory_A .memory_A(
+
+)
+memory_B .memory_B(
+
+)
+input_buff .input_buff(
+
+)
+output_buff .output_buff(
+
+)
+PE_array .PE_array(
+
+)
+ReLU .ReLU(
+
+)
+
+endmodule
