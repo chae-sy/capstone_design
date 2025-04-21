3575ddb222efada948df6ac2ee0044fbc7d080c6
milkaroo <thlrla03@naver.com>
2025-04-21 18:28:52 +0900

    Update controller.v

--- Verilog/controller.v
+++ Verilog/controller.v
@@ -1,25 +1,41 @@
 //controller
-module controller (
-
+module controller
+(
+    input   wire                clk,
+    input   wire                rst_n; 
 )
 
-memory_A .memory_A(
 
-)
-memory_B .memory_B(
+    mem_A       u_mem_A
+    (
+        .clk                        (clk),
+        .rst_n                      (rst_n),
+    );
+    mem_B       u_mem_B
+    (
+        .clk                        (clk),
+        .rst_n                      (rst_n),
+    );
+    in_buff     u_in_buff
+    (
+        .clk                        (clk),
+        .rst_n                      (rst_n),
+    );
+    out_buff    u_out_buff
+    (
+        .clk                        (clk),
+        .rst_n                      (rst_n),
+    );
+    PE_array    u_PE_array
+    (
+        .clk                        (clk),
+        .rst_n                      (rst_n),
+    );
+    ReLU        u_ReLU
+    (
+        .clk                        (clk),
+        .rst_n                      (rst_n),
+    );
 
-)
-input_buff .input_buff(
-
-)
-output_buff .output_buff(
-
-)
-PE_array .PE_array(
-
-)
-ReLU .ReLU(
-
-)
 
 endmodule
