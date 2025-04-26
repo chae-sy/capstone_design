d24e49fb5a1e9d8a18d8cc013c427161e55ee1cd
chae-sy <sychaeee@g.skku.edu>
2025-04-26 15:00:06 +0000

    training loop completed

--- CNN_subpixel_render/main.py
+++ CNN_subpixel_render/main.py
@@ -2,30 +2,39 @@ import torch
 from torch.utils.data import DataLoader
 import numpy as np
 from model import SPRNN
-from train import train_model
+from train import train_model, evaluate_model
 from dataset import SubpixelDataset, DIV2KDataset
 import torchvision.transforms as transforms
 import random
 
-def generate_fake_image_tensor(H, W):
-    return torch.rand(1, H, W)
+# def generate_fake_image_tensor(H, W):
+#     return torch.rand(1, H, W)
 
-def build_dataset(num_samples=100, H=256, W=256):
-    dataset = []
-    for _ in range(num_samples):
-        Ir = generate_fake_image_tensor(H, W)
-        Ig = generate_fake_image_tensor(H, W)
-        Ib = generate_fake_image_tensor(H, W)
+# def build_dataset(num_samples=100, H=256, W=256):
+#     dataset = []
+#     for _ in range(num_samples):
+#         Ir = generate_fake_image_tensor(H, W)
+#         Ig = generate_fake_image_tensor(H, W)
+#         Ib = generate_fake_image_tensor(H, W)
 
-        Pr, Pg, Pb = generate_pixel_masks(H, W)
+#         Pr, Pg, Pb = generate_pixel_masks(H, W)
 
-        dataset.append((Ir, Ig, Ib, Pr, Pg, Pb))
-    return dataset
+#         dataset.append((Ir, Ig, Ib, Pr, Pg, Pb))
+#     return dataset
+
+def save_model(model):
+        month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][time.localtime().tm_mon - 1]
+        date = time.localtime().tm_mday
+        today = f'{month}{date}'
+        now_time = f'{time.localtime().tm_hour}{time.localtime().tm_min}'
+        file_name = f'model_{today}_{now_time}.pt'
+        torch.save(model.state_dict(), file_name)
+        print('Model saved:', file_name)
 
 if __name__ == "__main__":
     # Settings
-    batch_size = 8
-    num_epochs = 10
+    b_size = 1
+    num_epochs = 30
     image_size = 100
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -48,21 +57,18 @@ if __name__ == "__main__":
     eval_dataset = DIV2KDataset(root_dir=div2k_test_dir, img_size=image_size, transform=transforms.ToTensor())  # Just normalization
 
     # Initialize dataloaders
-    train_loader = DataLoader(SubpixelDataset(train_dataset), batch_size=16, shuffle=True, num_workers=4, pin_memory=True)
-    valid_loader = DataLoader(SubpixelDataset(valid_dataset), batch_size=16, shuffle=False, num_workers=4, pin_memory=True)
-    eval_loader = DataLoader(SubpixelDataset(eval_dataset), batch_size=1, shuffle=False, num_workers=4, pin_memory=True)
-
-    # # Load dataset (replace with real DIV2K data later)
-    # train_data = build_dataset(100, image_size, image_size)
-    # val_data = build_dataset(20, image_size, image_size)
-    # test_data 
-    # train_loader = DataLoader(SubpixelDataset(train_data), batch_size=batch_size, shuffle=True)
-    # val_loader = DataLoader(SubpixelDataset(val_data), batch_size=batch_size)
-    # test_loader = 
-
+    train_loader = DataLoader(SubpixelDataset(train_dataset), batch_size=b_size, shuffle=True, num_workers=4, pin_memory=True)
+    valid_loader = DataLoader(SubpixelDataset(valid_dataset), batch_size=b_size, shuffle=False, num_workers=4, pin_memory=True)
+    eval_loader = DataLoader(SubpixelDataset(eval_dataset), batch_size=b_size, shuffle=False, num_workers=4, pin_memory=True)
 
     # Model
     model = SPRNN()
 
     # Train
     train_model(model, train_loader, valid_loader, epochs=num_epochs, device=device)
+
+    # Evaluate
+    evaluate_model(model, eval_loader)
+
+    # Save model
+    save_model(model)
