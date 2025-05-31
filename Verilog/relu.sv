cfdb263386a74c3bed25effc1d69ca3928cb446e
chae-sy <synnchae@gmail.com>
2025-05-31 14:29:27 +0900

    relu

--- Verilog/relu.sv
+++ Verilog/relu.sv
@@ -32,13 +32,19 @@ module bias_relu #(
     parameter WIDTH_L5_IN_IL = 8,
     parameter WIDTH_L5_OUT_IL = 0,
     parameter WIDTH_L5_ZERO_POINT = 0,
-    parameter WIDTH_L5_SCALE = 0.2
+    parameter WIDTH_L5_SCALE = 0.2,
+
+    parameter WIDTH_L6_IN_IL = 8,
+    parameter WIDTH_L6_OUT_IL = 0,
+    parameter WIDTH_L6_ZERO_POINT = 0,
+    parameter WIDTH_L6  _SCALE = 0.2
     
 )
 (
-    input                                           relu_on,
+    input                                           relu_en,
+    output   reg                                    relu_done,
     input   [2:0]                                   layer_state,
-    input signed [WIDTH_IN_DATA-1:0]     data_in,
+    input signed [WIDTH_IN_DATA-1:0]                data_in,
     input  signed [WIDTH_BIAS-1:0]                  bias,
   
     output  reg [WIDTH_OUT_DATA-1:0]                data_out
@@ -60,6 +66,9 @@ module bias_relu #(
   localparam WIDTH_L5_IN_FL = WIDTH_IN_DATA-WIDTH_L5_IN_IL-1;
   localparam WIDTH_L5_OUT_FL = WIDTH_OUT_DATA-WIDTH_L5_OUT_IL-1;
 
+  localparam WIDTH_L6_IN_FL = WIDTH_IN_DATA-WIDTH_L6_IN_IL-1;
+  localparam WIDTH_L6_OUT_FL = WIDTH_OUT_DATA-WIDTH_L6_OUT_IL-1;
+
   // precompute quantized threshold for RELU_MAX_VAL
   localparam real relu_q_real_l1 = (RELU_MAX_VAL * (2.0**WIDTH_L1_IN_FL)) / WIDTH_L1_SCALE;
   localparam int  relu_q_l1 = $rtoi(relu_q_real_l1 + 0.5) + WIDTH_L1_ZERO_POINT;
@@ -71,178 +80,115 @@ module bias_relu #(
   localparam int  relu_q_l4 = $rtoi(relu_q_real_l4 + 0.5) + WIDTH_L4_ZERO_POINT;
   localparam real relu_q_real_l5 = (RELU_MAX_VAL * (2.0**WIDTH_L5_IN_FL)) / WIDTH_L5_SCALE;
   localparam int  relu_q_l5 = $rtoi(relu_q_real_l5 + 0.5) + WIDTH_L5_ZERO_POINT;
-  
+  localparam real relu_q_real_l6 = (RELU_MAX_VAL * (2.0**WIDTH_L6_IN_FL)) / WIDTH_L6_SCALE;
+  localparam int  relu_q_l6 = $rtoi(relu_q_real_l6 + 0.5) + WIDTH_L6_ZERO_POINT;
+
+  // bias를 더한 후 RELU(또는 no_relu) 처리. 동시에 relu_done 신호를 생성.
   wire signed [WIDTH_BIAS-1:0] data_extended = 
         { { WIDTH_BIAS-WIDTH_IN_DATA { data_in[WIDTH_IN_DATA-1] } },
           data_in
         } << (WIDTH_BIAS - WIDTH_IN_DATA);
 
-     always @(*) begin
-        case (layer_state)
-          3'b001: begin  // L1
-            if (relu_on) begin
-              if (data_extended[WIDTH_BIAS-1])
-                data_out = WIDTH_L1_ZERO_POINT;
-              else if (data_extended > relu_q_l1)
-                data_out = relu_q_l1;
-              else
-                data_out = data_extended[WIDTH_IN_DATA - WIDTH_L1_IN_IL - 1 +: WIDTH_OUT_DATA];
-            end else begin
-              // if ReLU off,  saturate + fixed-point slicing
-              if (data_extended[WIDTH_BIAS-1]) begin
-                if (data_extended < -(2**(WIDTH_OUT_DATA-1) << (WIDTH_BIAS-WIDTH_L1_IN_IL-1-WIDTH_L1_OUT_FL)))
-                  data_out = WIDTH_L1_ZERO_POINT;
-                else
-                  data_out = {1'b1,
-                                  data_extended[WIDTH_BIAS-WIDTH_L1_IN_IL-1+WIDTH_L1_OUT_IL:
-                                                WIDTH_BIAS-WIDTH_L1_IN_IL-1],
-                                  data_extended[WIDTH_BIAS-WIDTH_L1_IN_IL-1:
-                                                WIDTH_BIAS-WIDTH_L1_IN_IL-1-WIDTH_L1_OUT_FL]};
-              end else begin
-                if (data_extended > ((2**(WIDTH_OUT_DATA-1)-1) << (WIDTH_BIAS-WIDTH_L1_IN_IL-1-WIDTH_L1_OUT_FL)))
-                  data_out = WIDTH_L1_ZERO_POINT + (2**WIDTH_OUT_DATA-1);
-                else
-                  data_out = {1'b0,
-                                  data_extended[WIDTH_BIAS-WIDTH_L1_IN_IL-1+WIDTH_L1_OUT_IL:
-                                                WIDTH_BIAS-WIDTH_L1_IN_IL-1],
-                                  data_extended[WIDTH_BIAS-WIDTH_L1_IN_IL-1:
-                                                WIDTH_BIAS-WIDTH_L1_IN_IL-1-WIDTH_L1_OUT_FL]};
-              end
-            end
-          end
-          3'b010: begin  // L2
-            if (relu_on) begin
-              if (data_extended[WIDTH_BIAS-1])
-                data_out = WIDTH_L2_ZERO_POINT;
-              else if (data_extended > relu_q_l2)
-                data_out = relu_q_l2;
-              else
-                data_out = data_extended[WIDTH_IN_DATA - WIDTH_L2_IN_IL - 1 +: WIDTH_OUT_DATA];
-            end else begin
-              //if ReLU off,  saturate + fixed-point slicing
-              if (data_extended[WIDTH_BIAS-1]) begin
-                if (data_extended < -(2**(WIDTH_OUT_DATA-1) << (WIDTH_BIAS-WIDTH_L2_IN_IL-1-WIDTH_L2_OUT_FL)))
-                  data_out = WIDTH_L2_ZERO_POINT;
-                else
-                  data_out = {1'b1,
-                                  data_extended[WIDTH_BIAS-WIDTH_L2_IN_IL-1+WIDTH_L2_OUT_IL:
-                                                WIDTH_BIAS-WIDTH_L2_IN_IL-1],
-                                  data_extended[WIDTH_BIAS-WIDTH_L2_IN_IL-1:
-                                                WIDTH_BIAS-WIDTH_L2_IN_IL-1-WIDTH_L2_OUT_FL]};
-              end else begin
-                if (data_extended > ((2**(WIDTH_OUT_DATA-1)-1) << (WIDTH_BIAS-WIDTH_L2_IN_IL-1-WIDTH_L2_OUT_FL)))
-                  data_out = WIDTH_L2_ZERO_POINT + (2**WIDTH_OUT_DATA-1);
-                else
-                  data_out = {1'b0,
-                                  data_extended[WIDTH_BIAS-WIDTH_L2_IN_IL-1+WIDTH_L2_OUT_IL:
-                                                WIDTH_BIAS-WIDTH_L2_IN_IL-1],
-                                  data_extended[WIDTH_BIAS-WIDTH_L2_IN_IL-1:
-                                                WIDTH_BIAS-WIDTH_L2_IN_IL-1-WIDTH_L2_OUT_FL]};
-              end
-            end
-          end
+  reg signed [WIDTH_BIAS-1:0] bias_added;
 
-          3'b011: begin  // L3
-            if (relu_on) begin
-              if (data_extended[WIDTH_BIAS-1])
-                data_out = WIDTH_L3_ZERO_POINT;
-              else if (data_extended > relu_q_l3)
-                data_out = relu_q_l3;
-              else
-                data_out = data_extended[WIDTH_IN_DATA - WIDTH_L3_IN_IL - 1 +: WIDTH_OUT_DATA];
-            end else begin
-              // if ReLU off,  saturate + fixed-point slicing
-              if (data_extended[WIDTH_BIAS-1]) begin
-                if (data_extended < -(2**(WIDTH_OUT_DATA-1) << (WIDTH_BIAS-WIDTH_L3_IN_IL-1-WIDTH_L3_OUT_FL)))
-                  data_out = WIDTH_L3_ZERO_POINT;
-                else
-                  data_out = {1'b1,
-                                  data_extended[WIDTH_BIAS-WIDTH_L3_IN_IL-1+WIDTH_L3_OUT_IL:
-                                                WIDTH_BIAS-WIDTH_L3_IN_IL-1],
-                                  data_extended[WIDTH_BIAS-WIDTH_L3_IN_IL-1:
-                                                WIDTH_BIAS-WIDTH_L3_IN_IL-1-WIDTH_L3_OUT_FL]};
-              end else begin
-                if (data_extended > ((2**(WIDTH_OUT_DATA-1)-1) << (WIDTH_BIAS-WIDTH_L3_IN_IL-1-WIDTH_L3_OUT_FL)))
-                  data_out = WIDTH_L3_ZERO_POINT + (2**WIDTH_OUT_DATA-1);
-                else
-                  data_out = {1'b0,
-                                  data_extended[WIDTH_BIAS-WIDTH_L3_IN_IL-1+WIDTH_L3_OUT_IL:
-                                                WIDTH_BIAS-WIDTH_L3_IN_IL-1],
-                                  data_extended[WIDTH_BIAS-WIDTH_L3_IN_IL-1:
-                                                WIDTH_BIAS-WIDTH_L3_IN_IL-1-WIDTH_L3_OUT_FL]};
-              end
-            end
-          end
+  always @(*) begin
+    // 기본값: 연산이 없거나 레이어 상태가 해당하지 않으면 data_out = 0, relu_done = 0
+    data_out   = {WIDTH_OUT_DATA{1'b0}};
+    relu_done  = 1'b0;
+    bias_added = {WIDTH_BIAS{1'b0}};
 
-          3'b100: begin  // L4
-            if (relu_on) begin
-              if (data_extended[WIDTH_BIAS-1])
-                data_out = WIDTH_L4_ZERO_POINT;
-              else if (data_extended > relu_q_l4)
-                data_out = relu_q_l4;
-              else
-                data_out = data_extended[WIDTH_IN_DATA - WIDTH_L4_IN_IL - 1 +: WIDTH_OUT_DATA];
-            end else begin
-              // if ReLU off,  saturate + fixed-point slicing
-              if (data_extended[WIDTH_BIAS-1]) begin
-                if (data_extended < -(2**(WIDTH_OUT_DATA-1) << (WIDTH_BIAS-WIDTH_L4_IN_IL-1-WIDTH_L4_OUT_FL)))
-                  data_out = WIDTH_L4_ZERO_POINT;
-                else
-                  data_out = {1'b1,
-                                  data_extended[WIDTH_BIAS-WIDTH_L4_IN_IL-1+WIDTH_L4_OUT_IL:
-                                                WIDTH_BIAS-WIDTH_L4_IN_IL-1],
-                                  data_extended[WIDTH_BIAS-WIDTH_L4_IN_IL-1:
-                                                WIDTH_BIAS-WIDTH_L4_IN_IL-1-WIDTH_L4_OUT_FL]};
-              end else begin
-                if (data_extended > ((2**(WIDTH_OUT_DATA-1)-1) << (WIDTH_BIAS-WIDTH_L4_IN_IL-1-WIDTH_L4_OUT_FL)))
-                  data_out = WIDTH_L4_ZERO_POINT + (2**WIDTH_OUT_DATA-1);
-                else
-                  data_out = {1'b0,
-                                  data_extended[WIDTH_BIAS-WIDTH_L4_IN_IL-1+WIDTH_L4_OUT_IL:
-                                                WIDTH_BIAS-WIDTH_L4_IN_IL-1],
-                                  data_extended[WIDTH_BIAS-WIDTH_L4_IN_IL-1:
-                                                WIDTH_BIAS-WIDTH_L4_IN_IL-1-WIDTH_L4_OUT_FL]};
-              end
-            end
-          end
+    if (relu_en) begin
+      case (layer_state)
+        3'b001: begin
+          bias_added = data_extended + bias;
+          // 음수라면 zero-point 출력
+          if (bias_added[WIDTH_BIAS-1])
+            data_out = WIDTH_L1_ZERO_POINT;
+          // Q값보다 크다면 클램핑
+          else if (bias_added > relu_q_l1)
+            data_out = relu_q_l1;
+          else
+            data_out = bias_added[WIDTH_IN_DATA - WIDTH_L1_IN_IL - 1 +: WIDTH_OUT_DATA];
+          relu_done = 1'b1;  // 연산 완료 신호
+        end
 
-          3'b101: begin  // L5
-            if (relu_on) begin
-              if (data_extended[WIDTH_BIAS-1])
-                data_out = WIDTH_L5_ZERO_POINT;
-              else if (data_extended > relu_q_l5)
-                data_out = relu_q_l5;
-              else
-                data_out = data_extended[WIDTH_IN_DATA - WIDTH_L5_IN_IL - 1 +: WIDTH_OUT_DATA];
-            end else begin
-              // if ReLU off,  saturate + fixed-point slicing
-              if (data_extended[WIDTH_BIAS-1]) begin
-                if (data_extended < -(2**(WIDTH_OUT_DATA-1) << (WIDTH_BIAS-WIDTH_L5_IN_IL-1-WIDTH_L5_OUT_FL)))
-                  data_out = WIDTH_L5_ZERO_POINT;
-                else
-                  data_out = {1'b1,
-                                  data_extended[WIDTH_BIAS-WIDTH_L5_IN_IL-1+WIDTH_L5_OUT_IL:
-                                                WIDTH_BIAS-WIDTH_L5_IN_IL-1],
-                                  data_extended[WIDTH_BIAS-WIDTH_L5_IN_IL-1:
-                                                WIDTH_BIAS-WIDTH_L5_IN_IL-1-WIDTH_L5_OUT_FL]};
-              end else begin
-                if (data_extended > ((2**(WIDTH_OUT_DATA-1)-1) << (WIDTH_BIAS-WIDTH_L5_IN_IL-1-WIDTH_L5_OUT_FL)))
-                  data_out = WIDTH_L5_ZERO_POINT + (2**WIDTH_OUT_DATA-1);
-                else
-                  data_out = {1'b0,
-                                  data_extended[WIDTH_BIAS-WIDTH_L5_IN_IL-1+WIDTH_L5_OUT_IL:
-                                                WIDTH_BIAS-WIDTH_L5_IN_IL-1],
-                                  data_extended[WIDTH_BIAS-WIDTH_L5_IN_IL-1:
-                                                WIDTH_BIAS-WIDTH_L5_IN_IL-1-WIDTH_L5_OUT_FL]};
-              end
-            end
+        3'b002: begin
+          bias_added = data_extended + bias;
+          if (bias_added[WIDTH_BIAS-1])
+            data_out = WIDTH_L1_ZERO_POINT;
+          else if (bias_added > relu_q_l1)
+            data_out = relu_q_l1;
+          else
+            data_out = bias_added[WIDTH_IN_DATA - WIDTH_L1_IN_IL - 1 +: WIDTH_OUT_DATA];
+          relu_done = 1'b1;
+        end
+
+        3'b003: begin
+          bias_added = data_extended + bias;
+          if (bias_added[WIDTH_BIAS-1])
+            data_out = WIDTH_L1_ZERO_POINT;
+          else if (bias_added > relu_q_l1)
+            data_out = relu_q_l1;
+          else
+            data_out = bias_added[WIDTH_IN_DATA - WIDTH_L1_IN_IL - 1 +: WIDTH_OUT_DATA];
+          relu_done = 1'b1;
+        end
+
+        3'b004: begin // maxpool 모드: bias 추가 없이 data_extended만 사용
+          if (data_extended[WIDTH_BIAS-1])
+            data_out = WIDTH_L1_ZERO_POINT;
+          else if (data_extended > relu_q_l1)
+            data_out = relu_q_l1;
+          else
+            data_out = data_extended[WIDTH_IN_DATA - WIDTH_L1_IN_IL - 1 +: WIDTH_OUT_DATA];
+          relu_done = 1'b1;
+        end
+
+        3'b005: begin 
+          bias_added = data_extended + bias;
+          if (bias_added[WIDTH_BIAS-1])
+            data_out = WIDTH_L1_ZERO_POINT;
+          else if (bias_added > relu_q_l1)
+            data_out = relu_q_l1;
+          else
+            data_out = bias_added[WIDTH_IN_DATA - WIDTH_L1_IN_IL - 1 +: WIDTH_OUT_DATA];
+          relu_done = 1'b1;
+        end
+
+        3'b006: begin // no_relu 모드: 양수/음수 그대로 양자화만 수행
+          bias_added = data_extended + bias;
+          if (bias_added[WIDTH_BIAS-1]) begin // 음수
+            if (bias_added < -(2**(WIDTH_OUT_DATA-1) << (WIDTH_BIAS-WIDTH_L1_IN_IL-1-WIDTH_L1_OUT_FL)))
+              data_out = WIDTH_L1_ZERO_POINT;
+            else
+              data_out = {1'b1,
+                          bias_added[WIDTH_BIAS-WIDTH_L1_IN_IL-1 + WIDTH_L1_OUT_IL:
+                                      WIDTH_BIAS-WIDTH_L1_IN_IL-1],
+                          bias_added[WIDTH_BIAS-WIDTH_L1_IN_IL-1:
+                                      WIDTH_BIAS-WIDTH_L1_IN_IL-1 - WIDTH_L1_OUT_FL]};
+          end else begin          // 양수
+            if (bias_added > ((2**(WIDTH_OUT_DATA-1)-1) << (WIDTH_BIAS-WIDTH_L1_IN_IL-1-WIDTH_L1_OUT_FL)))
+              data_out = WIDTH_L1_ZERO_POINT + (2**WIDTH_OUT_DATA-1);
+            else
+              data_out = {1'b0,
+                          bias_added[WIDTH_BIAS-WIDTH_L1_IN_IL-1 + WIDTH_L1_OUT_IL:
+                                      WIDTH_BIAS-WIDTH_L1_IN_IL-1],
+                          bias_added[WIDTH_BIAS-WIDTH_L1_IN_IL-1:
+                                      WIDTH_BIAS-WIDTH_L1_IN_IL-1 - WIDTH_L1_OUT_FL]};
           end
-          
-          default: data_out = {WIDTH_OUT_DATA{1'b0}};
-        endcase
-      end
-    
+          relu_done = 1'b1;
+        end
+
+        default: begin
+          data_out  = {WIDTH_OUT_DATA{1'b0}};
+          relu_done = 1'b0;
+        end
+      endcase
+    end
+    // relu_en = 0인 경우, data_out와 relu_done은 모두 0 (위에서 기본값으로 할당됨)
+  end
 
 endmodule
+
 
 
