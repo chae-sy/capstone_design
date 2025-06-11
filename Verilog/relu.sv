197a6a290a831793a4a72393e1a7536f95d8e784
chae-sy <synnchae@gmail.com>
2025-06-11 09:25:09 +0900

    Update relu.sv

--- Verilog/relu.sv
+++ Verilog/relu.sv
@@ -11,34 +11,22 @@ module bias_relu #(
     
     parameter WIDTH_L1_IN_IL = 9,
     parameter WIDTH_L1_OUT_IL = 1,
-    parameter WIDTH_L1_ZERO_POINT = -1,
-    parameter WIDTH_L1_SCALE = 0.2,
     
     parameter WIDTH_L2_IN_IL = 7,
     parameter WIDTH_L2_OUT_IL = 1,
-    parameter WIDTH_L2_ZERO_POINT = 0,
-    parameter WIDTH_L2_SCALE = 0.2,
     
     parameter WIDTH_L3_IN_IL = 12,
     parameter WIDTH_L3_OUT_IL = 1,
-    parameter WIDTH_L3_ZERO_POINT = 0,
-    parameter WIDTH_L3_SCALE = 0.2,
     
     parameter WIDTH_L4_IN_IL = 7,
     parameter WIDTH_L4_OUT_IL = 0,
-    parameter WIDTH_L4_ZERO_POINT = 0,
-    parameter WIDTH_L4_SCALE = 0.2,
-    
+     
     parameter WIDTH_L5_IN_IL = 8,
     parameter WIDTH_L5_OUT_IL = 0,
-    parameter WIDTH_L5_ZERO_POINT = 0,
-    parameter WIDTH_L5_SCALE = 0.2,
-
+    
     parameter WIDTH_L6_IN_IL = 8,
     parameter WIDTH_L6_OUT_IL = 0,
-    parameter WIDTH_L6_ZERO_POINT = 0,
-    parameter WIDTH_L6_SCALE = 0.2
-    
+      
 )
 (
     input   wire                                   relu_en,
@@ -68,19 +56,6 @@ module bias_relu #(
   localparam WIDTH_L6_IN_FL = WIDTH_IN_DATA-WIDTH_L6_IN_IL-1;
   localparam WIDTH_L6_OUT_FL = WIDTH_OUT_DATA-WIDTH_L6_OUT_IL-1;
 
-  // precompute quantized threshold for RELU_MAX_VAL
-  localparam real relu_q_real_l1 = (RELU_MAX_VAL * (2.0**WIDTH_L1_IN_FL)) / WIDTH_L1_SCALE;
-  localparam int  relu_q_l1 = $rtoi(relu_q_real_l1 + 0.5) + WIDTH_L1_ZERO_POINT;
-  localparam real relu_q_real_l2 = (RELU_MAX_VAL * (2.0**WIDTH_L2_IN_FL)) / WIDTH_L2_SCALE;
-  localparam int  relu_q_l2 = $rtoi(relu_q_real_l2 + 0.5) + WIDTH_L2_ZERO_POINT;
-  localparam real relu_q_real_l3 = (RELU_MAX_VAL * (2.0**WIDTH_L3_IN_FL)) / WIDTH_L3_SCALE;
-  localparam int  relu_q_l3 = $rtoi(relu_q_real_l3 + 0.5) + WIDTH_L3_ZERO_POINT;
-  localparam real relu_q_real_l4 = (RELU_MAX_VAL * (2.0**WIDTH_L4_IN_FL)) / WIDTH_L4_SCALE;
-  localparam int  relu_q_l4 = $rtoi(relu_q_real_l4 + 0.5) + WIDTH_L4_ZERO_POINT;
-  localparam real relu_q_real_l5 = (RELU_MAX_VAL * (2.0**WIDTH_L5_IN_FL)) / WIDTH_L5_SCALE;
-  localparam int  relu_q_l5 = $rtoi(relu_q_real_l5 + 0.5) + WIDTH_L5_ZERO_POINT;
-  localparam real relu_q_real_l6 = (RELU_MAX_VAL * (2.0**WIDTH_L6_IN_FL)) / WIDTH_L6_SCALE;
-  localparam int  relu_q_l6 = $rtoi(relu_q_real_l6 + 0.5) + WIDTH_L6_ZERO_POINT;
 
   // bias를 더한 후 RELU(또는 no_relu) 처리. 동시에 relu_done 신호를 생성.
   wire signed [WIDTH_BIAS-1:0] data_extended = 
@@ -102,79 +77,85 @@ module bias_relu #(
           bias_added = data_extended + bias;
           // 음수라면 zero-point 출력
           if (bias_added[WIDTH_BIAS-1])
-            data_out = WIDTH_L1_ZERO_POINT;
+            data_out = {WIDTH_OUT_DATA{1'b0}};
           // Q값보다 크다면 클램핑
-          else if (bias_added > relu_q_l1)
-            data_out = relu_q_l1;
+          else if (bias_added > RELU_MAX_VAL*(2**(WIDTH_L1_NORM_FL)))
+            data_out = RELU_MAX_VAL <<< WIDTH_L1_O_FL;
           else
-            data_out = bias_added[WIDTH_IN_DATA - WIDTH_L1_IN_IL - 1 +: WIDTH_OUT_DATA];
+            data_out = {1'b0, bias_added[WIDTH_L1_NORM_FL+WIDTH_L1_O_IL-1:WIDTH_L1_NORM_FL], 
+                        bias_added[WIDTH_L1_NORM_FL-1:WIDTH_L1_NORM_FL-WIDTH_L1_O_FL]};
           relu_done = 1'b1;  // 연산 완료 신호
         end
 
         3'd2: begin
           bias_added = data_extended + bias;
+          // 음수라면 zero-point 출력
           if (bias_added[WIDTH_BIAS-1])
-            data_out = WIDTH_L1_ZERO_POINT;
-          else if (bias_added > relu_q_l1)
-            data_out = relu_q_l1;
+            data_out = {WIDTH_OUT_DATA{1'b0}};
+          // Q값보다 크다면 클램핑
+          else if (bias_added > RELU_MAX_VAL*(2**(WIDTH_L2_NORM_FL)))
+            data_out = RELU_MAX_VAL <<< WIDTH_L2_O_FL;
           else
-            data_out = bias_added[WIDTH_IN_DATA - WIDTH_L1_IN_IL - 1 +: WIDTH_OUT_DATA];
-          relu_done = 1'b1;
+            data_out = {1'b0, bias_added[WIDTH_L2_NORM_FL+WIDTH_L2_O_IL-1:WIDTH_L2_NORM_FL], 
+                        bias_added[WIDTH_L2_NORM_FL-1:WIDTH_L2_NORM_FL-WIDTH_L2_O_FL]};
+          relu_done = 1'b1;  // 연산 완료 신호
         end
 
         3'd3: begin
           bias_added = data_extended + bias;
+          // 음수라면 zero-point 출력
           if (bias_added[WIDTH_BIAS-1])
-            data_out = WIDTH_L1_ZERO_POINT;
-          else if (bias_added > relu_q_l1)
-            data_out = relu_q_l1;
+            data_out = {WIDTH_OUT_DATA{1'b0}};
+          // Q값보다 크다면 클램핑
+          else if (bias_added > RELU_MAX_VAL*(2**(WIDTH_L3_NORM_FL)))
+            data_out = RELU_MAX_VAL <<< WIDTH_L3_O_FL;
           else
-            data_out = bias_added[WIDTH_IN_DATA - WIDTH_L1_IN_IL - 1 +: WIDTH_OUT_DATA];
-          relu_done = 1'b1;
+            data_out = {1'b0, bias_added[WIDTH_L3_NORM_FL+WIDTH_L3_O_IL-1:WIDTH_L3_NORM_FL], 
+                        bias_added[WIDTH_L3_NORM_FL-1:WIDTH_L3_NORM_FL-WIDTH_L3_O_FL]};
+          relu_done = 1'b1;  // 연산 완료 신호
         end
 
         3'd4: begin // maxpool 모드: bias 추가 없이 data_extended만 사용
+          // 음수라면 zero-point 출력
           if (data_extended[WIDTH_BIAS-1])
-            data_out = WIDTH_L1_ZERO_POINT;
-          else if (data_extended > relu_q_l1)
-            data_out = relu_q_l1;
+            data_out = {WIDTH_OUT_DATA{1'b0}};
+          // Q값보다 크다면 클램핑
+          else if (data_extended > RELU_MAX_VAL*(2**(WIDTH_L3_NORM_FL)))
+            data_out = RELU_MAX_VAL <<< WIDTH_L3_O_FL;
           else
-            data_out = data_extended[WIDTH_IN_DATA - WIDTH_L1_IN_IL - 1 +: WIDTH_OUT_DATA];
-          relu_done = 1'b1;
+            data_out = {1'b0, data_extended[WIDTH_L3_NORM_FL+WIDTH_L3_O_IL-1:WIDTH_L3_NORM_FL], 
+                        data_extended[WIDTH_L3_NORM_FL-1:WIDTH_L3_NORM_FL-WIDTH_L3_O_FL]};
+          relu_done = 1'b1;  // 연산 완료 신호
         end
 
         3'd5: begin 
           bias_added = data_extended + bias;
+          // 음수라면 zero-point 출력
           if (bias_added[WIDTH_BIAS-1])
-            data_out = WIDTH_L1_ZERO_POINT;
-          else if (bias_added > relu_q_l1)
-            data_out = relu_q_l1;
+            data_out = {WIDTH_OUT_DATA{1'b0}};
+          // Q값보다 크다면 클램핑
+          else if (bias_added > RELU_MAX_VAL*(2**(WIDTH_L5_NORM_FL)))
+            data_out = RELU_MAX_VAL <<< WIDTH_L5_O_FL;
           else
-            data_out = bias_added[WIDTH_IN_DATA - WIDTH_L1_IN_IL - 1 +: WIDTH_OUT_DATA];
-          relu_done = 1'b1;
+            data_out = {1'b0, bias_added[WIDTH_L5_NORM_FL+WIDTH_L5_O_IL-1:WIDTH_L5_NORM_FL], 
+                        bias_added[WIDTH_L5_NORM_FL-1:WIDTH_L5_NORM_FL-WIDTH_L5_O_FL]};
+          relu_done = 1'b1;  // 연산 완료 신호
         end
 
         3'd6: begin // no_relu 모드: 양수/음수 그대로 양자화만 수행
           bias_added = data_extended + bias;
-          if (bias_added[WIDTH_BIAS-1]) begin // 음수
-            if (bias_added < -(2**(WIDTH_OUT_DATA-1) << (WIDTH_BIAS-WIDTH_L1_IN_IL-1-WIDTH_L1_OUT_FL)))
-              data_out = WIDTH_L1_ZERO_POINT;
-            else
-              data_out = {1'b1,
-                          bias_added[WIDTH_BIAS-WIDTH_L1_IN_IL-1 + WIDTH_L1_OUT_IL:
-                                      WIDTH_BIAS-WIDTH_L1_IN_IL-1],
-                          bias_added[WIDTH_BIAS-WIDTH_L1_IN_IL-1:
-                                      WIDTH_BIAS-WIDTH_L1_IN_IL-1 - WIDTH_L1_OUT_FL]};
-          end else begin          // 양수
-            if (bias_added > ((2**(WIDTH_OUT_DATA-1)-1) << (WIDTH_BIAS-WIDTH_L1_IN_IL-1-WIDTH_L1_OUT_FL)))
-              data_out = WIDTH_L1_ZERO_POINT + (2**WIDTH_OUT_DATA-1);
-            else
-              data_out = {1'b0,
-                          bias_added[WIDTH_BIAS-WIDTH_L1_IN_IL-1 + WIDTH_L1_OUT_IL:
-                                      WIDTH_BIAS-WIDTH_L1_IN_IL-1],
-                          bias_added[WIDTH_BIAS-WIDTH_L1_IN_IL-1:
-                                      WIDTH_BIAS-WIDTH_L1_IN_IL-1 - WIDTH_L1_OUT_FL]};
-          end
+          if(bias_added[WIDTH_NORM_O_DATA-1]) begin
+                        if(bias_added < -(2**(WIDTH_L6_O_IL+WIDTH_L6_NORM_FL))) layer_out = 8'b10000000;
+                        else if (WIDTH_L6_O_IL == 0) layer_out = {1'b0, bias_added[WIDTH_L6_NORM_FL-1:WIDTH_L6_NORM_FL-WIDTH_L6_O_FL]};
+                        else layer_out = {1'b0, bias_added[WIDTH_L6_NORM_FL+WIDTH_L6_O_IL-1:WIDTH_L6_NORM_FL], 
+                                      bias_added[WIDTH_L6_NORM_FL-1:WIDTH_L6_NORM_FL-WIDTH_L6_O_FL]};
+                    end
+                    else begin
+                        if(bias_added > 2**(WIDTH_L6_O_IL+WIDTH_L6_NORM_FL)-1) layer_out = 8'b01111111;
+                        else if (WIDTH_L6_O_IL == 0) layer_out = {1'b0, bias_added[WIDTH_L6_NORM_FL-1:WIDTH_L6_NORM_FL-WIDTH_L6_O_FL]};
+                        else layer_out = {1'b0, bias_added[WIDTH_L6_NORM_FL+WIDTH_L6_O_IL-1:WIDTH_L6_NORM_FL], 
+                                      bias_added[WIDTH_L6_NORM_FL-1:WIDTH_L6_NORM_FL-WIDTH_L6_O_FL]};
+                    end
           relu_done = 1'b1;
         end
 
